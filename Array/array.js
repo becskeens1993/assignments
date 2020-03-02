2feb008c51e4b5c6639527b2026f94ddd4f412ef
becskeens1993 <61669910+becskeens1993@users.noreply.github.com>
2020-03-01 19:59:40 -0600

    upload
    update

--- Array/array.js
+++ Array/array.js
@@ -1,21 +1,28 @@
-var x1 = -15
-var x2 = 15
-var x3 = 40
-var x4 = 28
-var x5 = 50
+var x = 60
+var y = 150
+var z = 40
 
-function setup(){
-    createCanvas(windowWidth,windowHeight);
-}
 
-function draw(){
-    background(102,102,255);
-    x1 += 1;
-    x2 += 1;
-    x3 += 1;
-    x4 += 1;
-    x5 += 1;
-    rect(40, 30, 100, 100);
-    rect(130, 120, 75, 75, 20);
-    rect(300, 200, 205, 205, 200, 195, 190, 185);
-}
+
+
+function centerCanvas() {
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight-height) / 2;
+    cnv.position(x,y);
+   
+   }
+   
+   function setup(){
+     cnv = createCanvas(810,720);
+     centerCanvas();
+   }
+
+   function draw(){
+    background (102,102,255);
+    x += 0.8;
+    y += 0.8;
+    z += 0.8;
+    rect(x,340,100,100);
+    rect(y,500,70,70);
+    rect(z,90,10,10);
+   }
